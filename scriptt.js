a290f5c775c905016183f289760ee52419fc4bba
alex campe <campesatoalexis@gmail.com>
2021-12-02 23:24:15 -0300

    Modif Vender y varias

--- scriptt.js
+++ scriptt.js
@@ -13,13 +13,15 @@ class Moneda{
 
 }
 
+
 let moneda1 = new Moneda (1,"Bitcoin", 11470000)
 let moneda2 = new Moneda (2,"Ethereum", 740000)
-let moneda3 = new Moneda (3,"Usdt", 185)
+let moneda3 = new Moneda (3,"Usdt", 190)
 
 moneda1.comision();
 moneda2.comision();
 moneda3.comision();
+
 
 const arrayCripto = [moneda1, moneda2, moneda3]
     console.log(arrayCripto)
@@ -34,9 +36,98 @@ arrayCripto.forEach(Moneda=> {
         <h3>${Moneda.nombre}</h3>
         <h4>${Moneda.precio}</h4>
     <input type="submit" class="" id="botonComprar${Moneda.nombre}" value="COMPRAR">
-    <input type="submit" class="" id="botonVender" value="VENDER">
+    <input type="submit" class="" id="botonVender${Moneda.nombre}" value="VENDER">
     
   `})
+
+
+let boton4  =  document.getElementById("botonVenderBitcoin")
+
+boton4.addEventListener("click", ()=>{ 
+    let inputValue = document.getElementById("dinero").value;
+    let btc = moneda1.precio 
+
+    function division(inputValue, btc){
+        return inputValue / btc
+    }
+    let resultado = division(inputValue, btc)
+
+
+    document.getElementById("monedaRecibida").innerHTML = ` Usted vendió ${resultado} BTC`; 
+
+    $('#boxComplete').animate({
+        height: "20px",
+        width: "300px"
+    }, {
+        duration: 1000,
+        easing: "linear",
+        complete: function(){
+            $(this).after("<p> La transaccion fue realizada con exito</p>")
+            
+            $('#boxComplete').remove()
+        }
+    })
+
+
+})
+
+let boton5  =  document.getElementById("botonVenderEthereum")
+
+boton5.addEventListener("click", ()=>{
+
+    let inputValue = document.getElementById("dinero").value;
+    let eth = moneda2.precio 
+
+    function division(inputValue, eth){
+        return inputValue / eth
+    }
+    let resultado = division(inputValue, eth)
+
+
+    document.getElementById("monedaRecibida").innerHTML = `Usted vendió ${resultado} ETH`; 
+
+    $('#boxComplete').animate({
+        height: "20px",
+        width: "300px"
+    }, {
+        duration: 1000,
+        easing: "linear",
+        complete: function(){
+            $(this).after("<p> La transaccion fue realizada con exito</p>")
+            
+            $('#boxComplete').remove()
+        }
+    })
+})
+
+let boton6  =  document.getElementById("botonVenderUsdt")
+
+boton6.addEventListener("click", ()=>{
+
+    let inputValue = document.getElementById("dinero").value;
+    let usdt = moneda3.precio 
+
+    function division(inputValue, usdt){
+        return inputValue / usdt
+    }
+    let resultado = division(inputValue, usdt)
+
+
+    document.getElementById("monedaRecibida").innerHTML = `Usted vendió ${resultado} USDT`; 
+    
+    $('#boxComplete').animate({
+        height: "20px",
+        width: "300px"
+    }, {
+        duration: 1000,
+        easing: "linear",
+        complete: function(){
+            $(this).after("<p> La transaccion fue realizada con exito</p>")
+            
+            $('#boxComplete').remove()
+        }
+    })
+})
 
 let boton1  =  document.getElementById("botonComprarBitcoin")
 
@@ -50,7 +141,7 @@ let boton1  =  document.getElementById("botonComprarBitcoin")
         let resultado = division(inputValue, btc)
 
 
-        document.getElementById("monedaRecibida").innerHTML = `${resultado} BTC`; 
+        document.getElementById("monedaRecibida").innerHTML = `Usted recibió ${resultado} BTC`; 
 
         $('#boxComplete').animate({
             height: "20px",
@@ -81,7 +172,7 @@ let boton2  =  document.getElementById("botonComprarEthereum")
         let resultado = division(inputValue, eth)
 
 
-        document.getElementById("monedaRecibida").innerHTML = `${resultado} ETH`; 
+        document.getElementById("monedaRecibida").innerHTML = `Usted recibió ${resultado} ETH`; 
 
         $('#boxComplete').animate({
             height: "20px",
@@ -110,7 +201,7 @@ let boton3  =  document.getElementById("botonComprarUsdt")
         let resultado = division(inputValue, usdt)
 
 
-        document.getElementById("monedaRecibida").innerHTML = `${resultado} USDT`; 
+        document.getElementById("monedaRecibida").innerHTML = `Usted recibió ${resultado} USDT`; 
         
         $('#boxComplete').animate({
             height: "20px",
@@ -237,13 +328,41 @@ $(() => {
 
 
 /*---------animacion----------*/
+
 $('#tituloAnimate').animate({
     "font-size":"40px",
-    "padding":"30px",
+    "padding-top":"10%",
+    "padding-bottom":"10%",
     },
     "slow",
     function(){
         console.log("animacionterminada")
     }
 ).fadeOut(5000).fadeIn(5000)
+
+/*----API-------*/
+let dolares = []
+let divDolares = document.getElementById("divDolares")
+
+fetch("https://criptoya.com/api/satoshitango/btc/ars")
+.then(response => response.json())
+.then(data => console.log(data));
+ 
+fetch("https://criptoya.com/api/satoshitango/eth/ars")
+.then(response => response.json())
+.then(data => console.log(data));
+
+fetch("https://criptoya.com/api/satoshitango/usdt/ars")
+.then(response => response.json())
+.then(data => {
+    let datosPasados = Object.entries(data)
+    datosPasados.forEach(dolar => {
+        if(dolar[0] == "totalAsk") {
+     divDolares.innerHTML += `
+        <p id="${dolar[0]}"> ${dolar[0]} ${dolar[1]}</p>
+    `
+        }
+    })}
+    );
+  
   
